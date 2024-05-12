e51232a1edc0accbdc5b3ab1c6923da03d011921
Agnieszka <agamjp@gmail.com>
2024-05-12 13:09:43 +0200

    lined notebook version

---
+++ lined_notebook.py
@@ -0,0 +1,36 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="a4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.set_text_color(0, 0, 0)
+    pdf.cell(w=0, h=13, txt=f"{row['Order']}. {row['Topic']}", align="L", ln=1)
+    pdf.line(10, 22, 200, 22)
+    pdf.set_font(family="Times", size=14)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt="Notes:", align="L", ln=1, )
+    for i in range(31, 282, 10):
+        pdf.line(10, i, 200, i)
+    pdf.ln(249)
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=11, txt=f"{row['Topic']} | Page 1", align="C", ln=1)
+    for p in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", size=14)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=11, txt="Notes:", align="L", ln=1)
+        for i in range(20, 281, 10):
+            pdf.line(10, i, 200, i)
+        pdf.ln(260)
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=11, txt=f"{row['Topic']} | Page {p + 2}", align="C", ln=1)
+pdf.output("lined.pdf")
+
--- plain_notebook.py
+++ plain_notebook.py
@@ -12,18 +12,22 @@ for index, row in topics.iterrows():
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0, h=14, txt=f"{row['Order']}. {row['Topic']}", align="L", ln=1)
     pdf.line(10, 20, 200, 20)
+
     pdf.set_font(family="Arial", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt="Notes:", align="L", ln=1)
+
     pdf.ln(240)
     pdf.set_font(family="Arial", size=10, style="I")
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=12, txt=f"{row['Topic']} | Page 1", align="R", ln=1)
+
     for p in range(row["Pages"]-1):
         pdf.add_page()
         pdf.set_font(family="Arial", size=12)
         pdf.set_text_color(100, 100, 100)
-        pdf.cell(w=0, h=0, txt="\n\nNotes:", align="L", ln=1)
+        pdf.cell(w=0, h=0, txt="Notes:", align="L", ln=1)
+        
         pdf.ln(266)
         pdf.set_font(family="Arial", size=10, style="I")
         pdf.set_text_color(180, 180, 180)
